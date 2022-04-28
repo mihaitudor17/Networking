f9afc20f95717e802f09bb64447ad3c29f83e37d
Mihai_Tudor17 <mihai.blaga@student.unitbv.ro>
2022-04-29 01:28:11 +0300

    Added a TCP encryption implementation

---
+++ TCP Crypt/Client/TCP-Client.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    
+    class TCP_Client
+    {
+        private static Random random = new Random();
+        public static string RandomString(int length)
+        {
+
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            return new string(Enumerable.Repeat(chars, length)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+        static void Connect(String server, String message)
+        {
+
+            try
+            {
+                Int32 port = 13000;
+                TcpClient client = new TcpClient(server, port);
+                NetworkStream stream = client.GetStream();
+                string key;
+                if (message.Length<6)
+                    key = RandomString(message.Length);
+                else
+                    key = RandomString(6);
+                Byte[] data = System.Text.Encoding.ASCII.GetBytes(key);
+                stream.Write(data, 0, data.Length);
+                Console.WriteLine("Key: {0}", key);
+
+                Thread.Sleep(100);
+
+                message=Crypt(message,key);
+                data = System.Text.Encoding.ASCII.GetBytes(message);
+                stream.Write(data, 0, data.Length);
+
+                stream.Close();
+                client.Close();
+            }
+            catch (ArgumentNullException e)
+            {
+                Console.WriteLine("ArgumentNullException: {0}", e);
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+        }
+        public static string Crypt(string message,string key)
+        {
+            char[] temp=key.ToCharArray();
+            Array.Sort(temp);
+            string keyCopy = new string(temp);
+            List<int> pos=new List<int>();
+            Dictionary<char, int> hash = new Dictionary<char, int>();
+            int find=-1;
+            foreach (char c in key)
+            {
+                if (hash.ContainsKey(c))
+                {
+                    hash[c] += 1;
+                    find = keyCopy.IndexOf(c);
+                    for (int j = 0; j < hash[c]; j++)
+                        find = keyCopy.IndexOf(c, find + 1);
+                }
+                else
+                {
+                    hash.Add(c, 0);
+                    find = keyCopy.IndexOf(c);
+                }
+
+                pos.Add(find);
+            }
+            List<List<char>> list = new List<List<char>>();
+            List<char> temp1 = new List<char>();
+            int i = 0;
+            foreach(char c in message)
+            {
+                temp1.Add(c);
+                if ((i+1)%key.Length==0)
+                {  
+                    list.Add(temp1);
+                    temp1=new List<char>();
+                    temp1.Clear();
+                }
+                i++;
+            }
+            if(temp1.Count>0)
+                list.Add(temp1);
+            while (list[list.Count - 1].Count < key.Length)
+            {
+                list[list.Count - 1].Add('\0');
+            }
+            message = "";
+            for(int k=0;k<key.Length;k++)
+            {
+                int index = pos.FindIndex(a => a == k);
+                for (int j = 0; j < list.Count; j++)
+                    message += list[j][index].ToString();
+            }
+            System.Console.Write("Crypted message: ");
+            System.Console.WriteLine( message);
+            return (message);
+        }
+        public static void Main()
+        {
+            string localAddr = "127.0.0.1";
+            bool ok = true;
+            while(ok)
+            {
+                System.Console.Write("Please input the message: ");
+                string message = System.Console.ReadLine();
+                System.Console.WriteLine("Message: "+message);
+                Connect(localAddr, message);
+                if (message == "Close connection")
+                    ok = false;
+            }
+        }
+    }
+}
---
+++ TCP Crypt/Server/TCP-Server.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class TCP_Server
+    {
+        public static void Connect()
+        {
+            string key=null;
+            string message=null;
+            TcpListener server = null;
+            bool ok = true;
+            try
+            {
+
+                Int32 port = 13000;
+                IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+
+
+                server = new TcpListener(localAddr, port);
+
+                server.Start();
+
+                Byte[] bytes = new Byte[256];
+                String data = null;
+
+                while (ok)
+                {
+                    Console.Write("Waiting for a connection... ");
+
+                    TcpClient client = server.AcceptTcpClient();
+                    Console.WriteLine("Connected!");
+
+                    data = null;
+
+                    NetworkStream stream = client.GetStream();
+
+                    int i;
+
+                    while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+                    {
+                        data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+                        if (key != null)
+                        {
+
+                            message = data.ToString();
+                            System.Console.WriteLine("Key: " + key);
+                            System.Console.WriteLine("Crypted message: " + message);
+                            message = Decrypt(key, message);
+                            System.Console.WriteLine("Decrypted message: " + message);
+                            if (message == "Close connection")
+                                ok = false;
+                            key = null;
+                            message = null;
+                        }
+                        else
+                            key= data;
+                    }
+                    client.Close();
+                }
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+            finally
+            {
+                server.Stop();
+            }
+        }
+        public static string Decrypt(string key,string message)
+        {
+            char[] temp = key.ToCharArray();
+            string decrypt = "";
+            Array.Sort(temp);
+            string keyCopy = new string(temp);
+            List<int> pos = new List<int>();
+            Dictionary<char, int> hash = new Dictionary<char, int>();
+            int find = -1;
+            foreach (char c in key)
+            {
+                if (hash.ContainsKey(c))
+                {
+                    hash[c] += 1;
+                    find = keyCopy.IndexOf(c);
+                    for (int j = 0; j < hash[c]; j++)
+                        find = keyCopy.IndexOf(c, find + 1);
+                }
+                else
+                {
+                    hash.Add(c, 0);
+                    find = keyCopy.IndexOf(c);
+                }
+
+                pos.Add(find);
+            }
+            List<List<char>>list = new List<List<char>>();
+            for(int i = 0; i < message.Length/key.Length; i++)
+            {
+                List<char> c = new List<char>();
+                for (int j = 0; j < key.Length; j++)
+                    c.Add('\0');
+                list.Add(c);
+            }
+            for (int i=0;i<key.Length;i++)
+            {
+                int index = pos.FindIndex(a => a == i);
+                string temp1 = message.Substring(i * (message.Length / key.Length), (message.Length / key.Length));
+                for (int j = 0; j < message.Length / key.Length; j++)
+                    list[j][index] = temp1[j];
+            }
+            for (int i = 0; i < list.Count; i++)
+                for (int j = 0; j < list[i].Count; j++)
+                    decrypt += list[i][j];
+            return decrypt;
+        }
+        public static void Main()
+        {
+
+            Connect();
+        }
+    }
+}
